368d27382a74e998d17c9a515181536fe8e3b9c5
Adam Argyle <atom@argyleink.com>
2015-05-26 16:37:36 -0700

    new bolt in current or new directory

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 #! /usr/bin/env node
-var shell = require("shelljs");
-var userArgs = process.argv.splice(2);
-var command = userArgs[0];
 
-console.log(command);
+var shell       = require('shelljs')
+	, asciify 		= require('asciify')
+	, chalk 			= require('chalk')
 
-shell.exec("echo Bolt project");
+	// non deps
+  , userArgs    = process.argv.splice(2)
+  , command     = userArgs[0] || 'new'
+	, dir   			= userArgs[1] || '.'
+  , custom_dir  = dir !== '.'
+;
+
+asciify('Bolt', {
+	font: 'alligator',
+	color: 'blue'
+}, function(err, res){ 
+	console.log(res);
+	create();
+});
+
+function create() {
+	console.log(
+		chalk.blue('new project')
+	);
+	shell.exec('git clone git@github.com:argyleink/Bolt.git ' + dir);
+
+	console.log('cleaning up');
+	shell.exec('rm -rf '+ (custom_dir ? dir : '.') +'/.git');
+
+	console.log(
+		chalk.green('done')
+	);
+
+	console.log('run '+ chalk.red.bold('npm i') +' from the new project root');
+}
