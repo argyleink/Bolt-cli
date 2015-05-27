1684ca6bed92a484361a58fd0f163dbaeb911c64
Adam Argyle <atom@argyleink.com>
2015-05-26 17:38:44 -0700

    fixing tabs

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 #! /usr/bin/env node
 
 var shell       = require('shelljs')
-	, asciify 		= require('asciify')
-	, chalk 			= require('chalk')
+  , asciify     = require('asciify')
+  , chalk       = require('chalk')
 
-	// non deps
+  // non deps
   , userArgs    = process.argv.splice(2)
   , command     = userArgs[0] || 'new'
-	, dir   			= userArgs[1] || '.'
+  , dir         = userArgs[1] || '.'
   , custom_dir  = dir !== '.'
 ;
 
 asciify('Bolt', {
-	font: 'alligator',
-	color: 'blue'
+  font: 'alligator',
+  color: 'blue'
 }, function(err, res){ 
-	console.log(res);
-	create();
+  console.log(res);
+  create();
 });
 
 function create() {
-	console.log(
-		chalk.blue('new project')
-	);
-	shell.exec('git clone https://github.com/argyleink/Bolt.git ' + dir);
+  console.log(
+    chalk.blue('new project')
+  );
+  shell.exec('git clone https://github.com/argyleink/Bolt.git ' + dir);
 
-	console.log('cleaning up');
-	shell.exec('rm -rf '+ (custom_dir ? dir : '.') +'/.git');
+  console.log('cleaning up');
+  shell.exec('rm -rf '+ (custom_dir ? dir : '.') +'/.git');
 
-	console.log(
-		chalk.green('done')
-	);
+  console.log(
+    chalk.green('done')
+  );
 
-	console.log('run '+ chalk.red.bold('npm i') +' from the new project root');
+  console.log('run '+ chalk.red.bold('npm i') +' from the new project root');
 }
